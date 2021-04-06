4d6735b7e6162560863eea6ef2c12b09f646225b
ben1012728 <ben1012728@gmail.com>
2021-04-06 16:42:16 +0900

    event 사용으로 click시 색깔바뀌는 기능 구현/ l,i잘 구분하자..

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-const age = prompt("How old are you")
 
-if(age >= 18 && age <=21 ){
-    console.log("you can drink, but becairful")
-} else if (age > 21){
-    console.log("go ahead")
-} else{
-    console.log("too young")
+const title = document.querySelector("#title")
+const BASE_COLOR ="rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+
+function handleClick() {
+    console.log(title.style.color)
+
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR) {
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
+}
+    
+function init() { 
+    title.style.color = BASE_COLOR;
+    title.addEventListener('click', handleClick)
 }
 
+init();
 
 
+/*
+const title = document.querySelector('#title')
+const BASE_COLOR = 'rgb(255,10, 255)';
+const OTHER_COLOR = '#fff'
+
+function handleClick(){
+   console.log(title.style.color)
+
+   const currentColor = title.style.color;
+   if (currentColor === BASE_COLOR) {
+       title.style.color = OTHER_COLOR;
+   } else {
+       title.style.color = BASE_COLOR;
+   }
+}
+
+function init() {
+    title.style.color = BASE_COLOR;
+    title.addEventListener('click', handleClick)
+}
+
+init();
+*/

488b2e3b601f63cad12c42f9ab20905e94a446a9
ben1012728 <ben1012728@gmail.com>
2021-04-14 00:49:36 +0900

    To Do List 완성!! API 연결에서 애먹었지만 컴퓨터가 지금 나의 위치를 불러오지 못해서 위도와 경도를 가져와서 직접 입력했더니. 작동한다.

--- 바닐라 JS로 chrom앱 만들기/weather.js
+++ 바닐라 JS로 chrom앱 만들기/weather.js
@@ -1,12 +1,19 @@
-const API_Key ="1d620b0d883ebc384e94d7dc7d13fc9b"
+const weather = document.querySelector(".js-weather");
+const API_Key ="2b0e48d6768db04e501b15deaf8b8b18"
 const COORDS = 'coords';
 
 function getWeather(lat, lng){
      fetch(
-         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_key}}`
-         );
+         `https://api.openweathermap.org/data/2.5/weather?lat=37.61776595036474&lon=127.0676463506765&appid=2b0e48d6768db04e501b15deaf8b8b18&units=metric`
+         ).then(function(respone){
+             return respone.json()
+         }) .then(function(json) {
+             const temperature = json.main.temp;
+             const place = json.name;
+             weather.innerText = `${temperature}@ ${place}`
+         });
 }
-
+//37.61776595036474, 127.0676463506765
 function saveCoords(coordsObj){
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
@@ -46,3 +53,52 @@ function init (){
 }
 
 init();
+
+/*
+const API_KEY="1d620b0d883ebc384e94d7dc7d13fc9b";
+
+const COORDS = 'coords'
+
+function getWeather(lat,lng){ 
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_key}`
+)};
+
+
+function saveCoords(coordsObj){ 
+   localStorage.setItem(COORDS,JSON.stringify(coordsObj)) 
+}
+
+function handleGeoSuccess(position){ 
+    const latitude=position.coords.latitude; 
+    const longitude=position.coords.longitude; 
+    const coordsObj={ latitude:latitude, longitude:longitude };
+}
+
+function handleGeoError(){ 
+    console.log ("Cant access geo location"); 
+}
+
+
+function askForCoords(){ 
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess,handleGeoError)
+
+}
+
+function loadCoords(){ 
+    const loadedCoords = localStorage.getItem(COORDS)
+     if(loadedCoords===null){ 
+         askForCoords(); 
+    } else { 
+        const parseCoords=JSON.parse(loadedCoords); 
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+
+}
+}
+
+function init(){ 
+    loadCoords(); 
+}
+
+
+init();
+*/

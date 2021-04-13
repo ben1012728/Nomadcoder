b5f096204d286d8c448d5c89923e5d894a8bb9d1
ben1012728 <ben1012728@gmail.com>
2021-04-14 01:01:27 +0900

    수정

--- 바닐라 JS로 chrom앱 만들기/gretting.js
+++ 바닐라 JS로 chrom앱 만들기/gretting.js
@@ -1,51 +1,4 @@
-/*
-const 
- form = document.querySelector(".js-form"),
- input = form.querySelector("input"),
- greeting = document.querySelector(".js-greetings");
 
-const 
-     USER_LS ="currentUser",
-     SHOWING_CN = "showing";
-
-function saveName(text){
-    localStorage.setItem(USER_LS, text);
-}
-function handlesubmit(event){
-     event.preventDefault();
-     const currentValue = input.value;
-     paintGreeting(currentValue);
-     saveName(currentValue);
-}
-
-function askForName(){
-     form.classList.add(SHOWING_CN);
-     form.addEventListener("submit", handleSubmit);
-}  
-
-function paintGreeting(text){
-    form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    greeting.innerText=`Hello ${text}`;
-}
-
-
- function loadName(){
-     const currentUser = localStorage.getItem(USER_LS);
-      if (currentUser === null) {
-        askForName();
-    } else{
-            paintGreeting(currentUser);
-    }
- }
- 
- function init() {
-    loadName();
- }
- 
- init();
-
-*/
 const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
   greeting = document.querySelector(".js-greetings");
@@ -71,7 +24,7 @@ function askForName() {
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
+  greeting.innerText = `안녕 ${text}`;
 }
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
